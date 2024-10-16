0e0b6b5b6f12e6a1f0d458474315561c59d78c51
SeaSkr <se.gonzaleza@duocuc.cl>
2024-10-15 21:34:31 -0300

    nose

--- software-biblioteca/app/Models/usuario.php
+++ software-biblioteca/app/Models/usuario.php
@@ -2,29 +2,10 @@
 
 namespace App\Models;
 
-// use Illuminate\Database\Eloquent\Factories\HasFactory;
-// use Illuminate\Database\Eloquent\Model;
-use Illuminate\Notifications\Notifiable;
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Usuario extends Authenticatable
+class Usuario extends Model
 {
-    use Notifiable;
-
-    protected $table = 'usuarios'; // Apunta a la tabla correcta
-
-    protected $fillable = [
-        'nombre', 'apellido', 'correo', 'contraseña', 'tipo_usuario',
-    ];
-
-    // Indicar que la columna de la contraseña es 'contraseña'
-    public function getAuthPassword()
-    {
-        return $this->contraseña;
-    }
-
-    protected $hidden = [
-        'contraseña', // Asegúrate de que la contraseña esté oculta
-        'remember_token',
-    ];
+    use HasFactory;
 }
