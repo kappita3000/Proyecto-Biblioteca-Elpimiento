f69b19871140d0adb45597fbc3f3d38278b7afff
Matias Mateluna <ma.mateluna@duocuc.cl>
2024-11-30 23:32:02 -0300

    Agregadas validaciones de correo y contraseña en formulario crear cuenta de usuario

--- software-biblioteca/resources/views/signin.blade.php
+++ software-biblioteca/resources/views/signin.blade.php
@@ -2,7 +2,6 @@
 <title>Crear cuenta</title>
 @section('content')
 
-    
 <div class="container w-20 p-4">
     <form action="{{ route('usuario') }}" method="POST">
         @csrf
@@ -22,9 +21,9 @@
         @endif
 
         <div class="mb-3">
-          <label for="correo" class="form-label">Correo electronico</label>
+          <label for="correo" class="form-label">Correo electrónico</label>
           <input type="email" class="form-control" id="correo" aria-describedby="emailHelp" name="correo" required autofocus>
-          <div id="emailHelp" class="form-text">Su correo electronico no sera distribuido de ninguna manera</div>
+          <span id="emailError" style="display: none; color: red;"></span>
         </div>
         <div class="row">
             <div class="col">
@@ -35,14 +34,15 @@
                 <label for="apellido" class="form-label">Apellido</label>
                 <input type="text" id="apellido" class="form-control" aria-label="Last name" name="apellido" required autofocus>
             </div>
-          </div>
+        </div>
 
-          <div class="mb-3">
+        <div class="mb-3">
             <label for="password" class="form-label">Contraseña</label>
             <div class="input-group">
                 <input type="password" class="form-control" id="password" name="contraseña" required autofocus>
                 <button class="btn btn-outline-secondary" type="button" id="togglePassword1">Mostrar</button>
             </div>
+            <span id="passwordError" style="display: none; color: red;"></span>
         </div>
         <div class="mb-3">
             <label for="confirmPassword" class="form-label">Repetir Contraseña</label>
@@ -50,14 +50,12 @@
                 <input type="password" class="form-control" id="confirmPassword" name="contraseña_confirmation" required autofocus>
                 <button class="btn btn-outline-secondary" type="button" id="togglePassword2">Mostrar</button>
             </div>
-            <div id="passwordHelp" class="form-text text-danger" style="display:none;">Las contraseñas no coinciden.</div>
+            <span id="confirmPasswordError" style="display: none; color: red;"></span>
         </div>
         
         <button type="submit" class="btn btn-primary" id="submitBtn" disabled>Ingresar</button>
-      </form>
+    </form>
 
-
-
-      <script src="{{ asset('js/signin.js') }}"></script>
+    <script src="{{ asset('js/signin.js') }}"></script>
 </div>
 @endsection
